cdb73e51351275f7530d04b29bc45a1352b543f1
Nicolas Baeza <ianbaezarivera@gmail.com>
2024-03-15 10:28:08 -0300

    Final appScript.js logic is added

--- src/js/appScript.js
+++ src/js/appScript.js
@@ -1,23 +1,41 @@
+const apiURL = "https://api.openai.com/v1/images/generations";
+const OPENAI_API_KEY = "";
 
 const formulario = document.querySelector(".formulario");
 const galeriaImagenes = document.querySelector(".galeria-imagenes");
 
-const OPENAI_API_KEY = "";
+let estaGenerandoImagen = false;
+
+const actualizarImagen = (arrayImagenes) => {
+    arrayImagenes.forEach((objImagen, indice) => {
+        const imagen = galeriaImagenes.querySelectorAll(".imagen")[indice];
+        const imagenElement = imagen.querySelector("img");
+        const btnDescargar = imagen.querySelector(".btn-descargar");
+
+        const iaGeneratedImg = `data:image/jpeg;base64,${objImagen.b64_json}`;
+        imagenElement.src = iaGeneratedImg;
+
+        imagenElement.onload = () => {
+            imagen.classList.remove("cargando");
+            btnDescargar.setAttribute("href", iaGeneratedImg);
+            btnDescargar.setAttribute("download", `${new Date().getTime()}.jpg`);
+        }
+    });
+}
 
 const generarImagenes = async (inputTextUsuario, cantidadImgUsuario) => {
     try {
-        const respuesta = await fetch("https://api.openai.com/v1/images/generations",{
+        const respuesta = await fetch( apiURL, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${OPENAI_API_KEY}`
+                "Content-Type" : "application/json",
+                "Authorization" : `Bearer ${OPENAI_API_KEY}`
             },
-            body: JSON.stringify({
-                // model : "dall-e-3",
-                prompt: inputTextUsuario,
-                n: parseInt(cantidadImgUsuario),
-                size: "512x512",
-                response_format: "b64_json"
+            body : JSON.stringify({
+                prompt : inputTextUsuario,
+                n : parseInt(cantidadImgUsuario),
+                size : "512x512",
+                response_format : "b64_json"
             })
         });
 
@@ -25,24 +43,30 @@ const generarImagenes = async (inputTextUsuario, cantidadImgUsuario) => {
             throw new Error("Error al generar imagenes!");
         }
 
-        const {data} = await respuesta.json();
+        const { data } = await respuesta.json();
         console.log(data);
+        actualizarImagen([...data]);
 
     } catch (error) {
         alert(error.message);
+    } finally {
+        estaGenerandoImagen = false;
     }
 }
 
 const handleFormSubmission = (e) => {
     e.preventDefault();
 
+    if(estaGenerandoImagen) return;
+    estaGenerandoImagen = true;
+
     const inputTextUsuario = e.srcElement[0].value;
     const cantidadImgUsuario = e.srcElement[1].value;
 
     const imagen = Array.from(
         {length : cantidadImgUsuario}, () => `
-        <div class="imagen">
-            <img src="/src/assets/images/fondo.jpg" 
+        <div class="imagen cargando">
+            <img src="/src/assets/images/loader.svg" 
                 alt="imagen">
 
             <a href="#" class="btn-descargar">
